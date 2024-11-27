a3a929eddd0cbd119882a683119cc1809e7cd465
MaxwellKJr <maxwellkjr@gmail.com>
2024-11-27 11:16:50 +0200

    Cleaning up prettier formatting

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      white: "#ebebeb",
+      black: "#111111",
+      h1: "#ff0077",
+      h2: "#ff006675",
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#ff0077",
+        secondary: "#ff006675",
+        "secondary-dark": "#301622",
+        "secondary-white": "#8f8f8f",
+        "background-dark": "#111111",
+        "card-background": "rgba(0, 0, 0, 0.562)",
+        "card-border": "#000000",
+      },
+      fontFamily: {
+        odibee: ["Odibee Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
