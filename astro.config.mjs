ddda2ac0696d9b9056881498bb3a04b47316032b
MaxwellKJr <maxwellkjr@gmail.com>
2025-01-02 23:42:32 +0200

    BUILD ERROR FIX - Add Netlify adapter and update dependencies in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import icon from 'astro-icon';
 
 import sanity from '@sanity/astro';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -21,5 +23,7 @@ export default defineConfig({
     react(),
     tailwind(),
     icon(),
-  ]
+  ],
+
+  adapter: netlify()
 });
