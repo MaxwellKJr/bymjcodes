fe58ad0ffe2d16933ede619242b3454adf2788ce
MaxwellKJr <maxwellkjr@gmail.com>
2025-02-23 11:35:27 +0200

    UI Updates and Bug fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,24 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      keyframes: {
+        line: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' }
+        },
+        glitch: {
+          '0%, 100%': { transform: 'translate(0)' },
+          '33%': { transform: 'translateX(-2px)' },
+          '66%': { transform: 'translateY(4px)' },
+        }
+      },
+      // TODO: Fix the half-baked glitch effect
+      animation: {
+        'line': 'line 1s ease-in-out forwards',
+        'glitch': 'glitch 240ms infinite',
+        'glitch-delayed-1': 'glitch 300ms infinite 100ms text-primary',
+        'glitch-delayed-2': 'glitch 300ms infinite 150ms text-secondary',
+      },
       colors: {
         primary: "#ff0098",
         secondary: "#ff006675",
